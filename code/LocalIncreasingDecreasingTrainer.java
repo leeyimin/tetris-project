5339f84c358a52af36d856cff612696642edfd08
Yi Min <imleeyimin@gmail.com>
2018-03-30 19:28:48 +0800

    add decreasing step, and try different parameters

--- code/LocalIncreasingDecreasingTrainer.java
+++ code/LocalIncreasingDecreasingTrainer.java
@@ -5,32 +5,45 @@ import java.util.function.Function;
 
 /**
  * Increases each coefficient until it stops improving, then move on to next coefficient. Lower the increment after one iteration of the features.
+ * After the increment reaches below epsilon, it repeats the steps but decreasing each coefficient instead.
+ *
  * Perturbation strategy to be improved.
  */
-public class LocalIncreasingTrainer extends Trainer{
+public class LocalIncreasingDecreasingTrainer extends Trainer{
 
-    static final int ITERATIONS = 50;
+    static final int ITERATIONS = 200;
+    static final double STARTING_INCREMENT = 16;
+    static final double EPSILON = 1.0;
+    static final double factor = 4.0; // multiply increment by 1/factor after one iteration of the features
+    static final int MOVES = 300;
 
     double bestResult = Double.MIN_VALUE;
     List<Double> bestCoefficient;
     int rounds;
-    double increment = 16;
+    double increment;
     double[][] resultsInRound;
+    int direction;
+
 
     int currentCoefficient;
 
-    public LocalIncreasingTrainer(List<Double> coefficients, List<Function<TestState, Double>> features) {
-        super(10000, coefficients, features);
+    Random random;
+
+    public LocalIncreasingDecreasingTrainer(List<Double> coefficients, List<Function<TestState, Double>> features) {
+        super(features.size()*50*100, coefficients, features);
         rounds = 0;
         resultsInRound = new double[ITERATIONS][2];
         bestCoefficient = new ArrayList<>(coefficients);
+        increment = STARTING_INCREMENT;
         currentCoefficient = 0;
+        direction = 1;
+        random = new Random();
         //coefficients.set(currentCoefficient, coefficients.get(currentCoefficient)+increment);
     }
 
     public void train(){
         for (int i = 0; i < this.numIterations; i++) {
-            double[] results = new Player(coefficients, features).simulate(1000);
+            double[] results = new Player(coefficients, features).simulate(MOVES);
             if(this.onSimulateDone(results)) break;
         }
     }
@@ -64,7 +77,8 @@ public class LocalIncreasingTrainer extends Trainer{
             double sum = 0;
 
             for (double[] r : resultsInRound){
-                sum+=r[0];
+                //penalise rounds with deaths
+                sum+=r[1] ==  Double.MAX_VALUE? r[0]/2 : r[0];
             }
 
             if(sum > bestResult){
@@ -77,26 +91,35 @@ public class LocalIncreasingTrainer extends Trainer{
                 currentCoefficient++;
                 if(currentCoefficient% features.size() == 0){
                     currentCoefficient = 0;
-                    increment /=2;
+                    increment /= factor;
                 }
-                if(increment < 1) {
-                    increment = 16;
+                if(Math.abs(increment) < EPSILON) {
+                    direction *= -1;
+                    increment = direction * STARTING_INCREMENT;
                     System.out.println();
                     System.out.println("BEST");
                     System.out.println(bestResult);
-
-                    for(int i =0;i<coefficients.size(); i++) {
+                    for (int i = 0; i < coefficients.size(); i++) {
                         System.out.print(bestCoefficient.get(i) + " ");
-                        bestCoefficient.set(i, bestCoefficient.get(i)/2);
                     }
 
                     System.out.println();
                     System.out.println();
 
+
+
+                    bestResult = Double.MIN_VALUE;
+
+                    //perturbation
+                    //TODO: perturbation strategy to be improved
+                    if(direction == 1){
                         return true;
-                    // perturbation strategy to be improved
-//                    bestResult = Double.MIN_VALUE;
-//                    coefficients = new ArrayList<>(bestCoefficient);
+//                        System.out.println("PERTURBATION");
+//                        for (int i = 0; i < coefficients.size(); i++) {
+//                            bestCoefficient.set(i, bestCoefficient.get(i) + (1-2*(random.nextInt()%2))*STARTING_INCREMENT );
+//                        }
+//                        coefficients = new ArrayList<>(bestCoefficient);
+                    }
                 }
                 else{
 
@@ -120,20 +143,8 @@ public class LocalIncreasingTrainer extends Trainer{
     public static void main(String args[]) {
         List<Double> coefficients = new ArrayList<>();
 
-        coefficients.add(0.0);
-        coefficients.add(0.0);
-        coefficients.add(0.0);
-        coefficients.add(0.0);
-        coefficients.add(0.0);
-        coefficients.add(0.0);
-        coefficients.add(0.0);
-        coefficients.add(0.0);
-        coefficients.add(0.0);
-        coefficients.add(0.0);
-        coefficients.add(0.0);
-        coefficients.add(0.0);
-
         List<Function<TestState, Double>> features = new ArrayList<>();
+
         features.add(Features::getBumpiness);
         features.add(Features::getTotalHeight);
         features.add(Features::getMaxHeight);
@@ -146,7 +157,25 @@ public class LocalIncreasingTrainer extends Trainer{
         features.add(Features::getNegativeOfRowsCleared);
         features.add(Features::hasPossibleDeathNextPiece);
         features.add(Features::getBlocksAboveHoles);
+        features.add(Features::getNumColsWithHoles);
+        features.add(Features::getNumRowsWithHoles);
+        features.add(Features::getFirstColHeight);
+        features.add(Features::getSecondColHeight);
+        features.add(Features::getThirdColHeight);
+        features.add(Features::getFourthColHeight);
+        features.add(Features::getFifthColHeight);
+        features.add(Features::getSixthColHeight);
+        features.add(Features::getSeventhColHeight);
+        features.add(Features::getEighthColHeight);
+        features.add(Features::getNinthColHeight);
+        features.add(Features::getTenthColHeight);
 
-        new LocalIncreasingTrainer(coefficients, features).train();
+        initialiseCoefficients(coefficients, features.size());
+
+        new LocalIncreasingDecreasingTrainer(coefficients, features).train();
+    }
+
+   public static void initialiseCoefficients(List<Double> coefficients, int size){
+        for(int i=0;i<size;i++) coefficients.add(0.0);
     }
 }
