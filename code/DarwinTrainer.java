71d85619a166005591b3c396f6c35b38927e0c5c
Zhi Xuan Chng <e0032135@u.nus.edu>
2018-04-04 21:32:10 +0800

    change DarwinTrainer to use LocalIncTrainer coefficients and allow negative weights.

--- code/DarwinTrainer.java
+++ code/DarwinTrainer.java
@@ -22,6 +22,13 @@ public class DarwinTrainer extends Trainer {
         System.out.println();
     }
 
+    /**
+     * Testing all coefficients to play the game. Top 2 fittest coefficients are tracked.
+     *
+     * @param playerCoefficients
+     * @param features
+     * @return integer array containing the index of the 2 fittest coefficients in playerCoefficients
+     */
     private static int[] oneGenerationRun(ArrayList<List<Double>> playerCoefficients,
                                           List<Function<TestState, Double>> features) {
 
@@ -36,9 +43,6 @@ public class DarwinTrainer extends Trainer {
         for (int playerIndex = 0; playerIndex < playerCoefficients.size(); playerIndex++) {
 
             currentPlayer = playerCoefficients.get(playerIndex);
-
-            System.out.println(currentPlayer);
-
             DarwinTrainer player = new DarwinTrainer(currentPlayer, features);
 
             currResult = player.getAverageResult();
@@ -51,7 +55,6 @@ public class DarwinTrainer extends Trainer {
                 secondHighScore = currResult;
                 secondBestPlayer = playerIndex;
             }
-
         }
 
         System.out.println("@@@@@@@@@@@@ BATTLE ROYALE RESULTS @@@@@@@@@@@@@");
@@ -66,6 +69,13 @@ public class DarwinTrainer extends Trainer {
         return resultPair;
     }
 
+    /**
+     *
+     *
+     * @param resultPair
+     * @param playerCoefficients
+     * @return  ArrayList of the mutated cross product of the previous 2 fittest coefficients
+     */
     private static  ArrayList<List<Double>> crossBreed(int[] resultPair, ArrayList<List<Double>> playerCoefficients) {
 
         List<Double> bestCoefficient = playerCoefficients.get(resultPair[0]);
@@ -83,16 +93,14 @@ public class DarwinTrainer extends Trainer {
 
         double mutatedCoefficient;
 
-
-
-        //Perfect cross breed
+        /**
+         *  Perfect cross breed
+         */
         for (int i = 0; i < perfectCount; i++) {
 
             childCoefficient = new ArrayList<Double>();
             for (int j = 0; j < bestCoefficient.size(); j++) {
                 parentCoin = ThreadLocalRandom.current().nextInt(0, 2);
-
-
 
                 if (parentCoin == 0) {
                     childCoefficient.add(bestCoefficient.get(j));
@@ -103,19 +111,21 @@ public class DarwinTrainer extends Trainer {
             newGeneration.add(childCoefficient);
         }
 
-        //stupid cross breed
+        /**
+         * Minor mutation cross breed
+         */
         for (int i = 0; i < stupidCount; i++) {
 
             childCoefficient = new ArrayList<Double>();
             for (int j = 0; j < bestCoefficient.size(); j++) {
                 parentCoin = ThreadLocalRandom.current().nextInt(0, 2);
-                mutationDice = ThreadLocalRandom.current().nextInt(0, 3);
+                mutationDice = ThreadLocalRandom.current().nextInt(0, 4);
 
                 if (parentCoin == 0) {
 
                     if (mutationDice == 0) {
                         mutatedCoefficient = bestCoefficient.get(j) +
-                                (ThreadLocalRandom.current().nextDouble(-0.3,0.3));
+                                (ThreadLocalRandom.current().nextDouble(-0.2,0.2));
                     } else {
                         mutatedCoefficient = bestCoefficient.get(j);
                     }
@@ -125,19 +135,21 @@ public class DarwinTrainer extends Trainer {
                 } else {
                     if (mutationDice == 0) {
                         mutatedCoefficient = secondCoefficient.get(j) +
-                                (ThreadLocalRandom.current().nextDouble(-0.3,0.3));
+                                (ThreadLocalRandom.current().nextDouble(-0.2,0.2));
                     } else {
                         mutatedCoefficient = secondCoefficient.get(j);
                     }
 
-                    childCoefficient.add(Math.max(mutatedCoefficient, 0));
+                    childCoefficient.add(mutatedCoefficient);
                 }
             }
             newGeneration.add(childCoefficient);
         }
 
 
-        //retarded crossbreed
+        /**
+         * Large mutation cross breed
+         */
         for (int i = 0; i < retardedCount; i++) {
 
             childCoefficient = new ArrayList<Double>();
@@ -149,7 +161,7 @@ public class DarwinTrainer extends Trainer {
 
                     if (mutationDice == 0) {
                         mutatedCoefficient = bestCoefficient.get(j) +
-                                (ThreadLocalRandom.current().nextDouble(-1,1));
+                                (ThreadLocalRandom.current().nextDouble(-2,2));
                     } else {
                         mutatedCoefficient = bestCoefficient.get(j);
                     }
@@ -159,12 +171,12 @@ public class DarwinTrainer extends Trainer {
                 } else {
                     if (mutationDice == 0) {
                         mutatedCoefficient = secondCoefficient.get(j) +
-                                (ThreadLocalRandom.current().nextDouble(-0.4,0.4));
+                                (ThreadLocalRandom.current().nextDouble(-2,2));
                     } else {
                         mutatedCoefficient = secondCoefficient.get(j);
                     }
 
-                    childCoefficient.add(Math.max(mutatedCoefficient, 0));
+                    childCoefficient.add(mutatedCoefficient);
                 }
             }
             newGeneration.add(childCoefficient);
@@ -176,35 +188,72 @@ public class DarwinTrainer extends Trainer {
     public static void main(String args[]) {
         ArrayList<List<Double>> playerCoefficients = new ArrayList<>();
         List<Function<TestState, Double>> features = new ArrayList<>();
+        List<Function<TestState, Double>> trimmedFeatures = new ArrayList<>();
         List<Double> oneSetCoefficients;
         int[] resultPair = new int[2];
 
-        final int numPlayers = 20;
+        final int numPlayers = 1000;
 
         //Adding features
-        features.add(Features::hasPossibleInevitableDeathNextPiece);
         features.add(Features::getNegativeOfRowsCleared);
-        features.add(Features::hasRightStep);
-        features.add(Features::getMeanAbsoluteDeviationOfTop);
-        features.add(Features::getTotalHeight);
-        features.add(Features::getNumOfSignificantTopDifference);
+        features.add(Features::getMaxHeight);
         features.add(Features::getNumHoles);
+        features.add(Features::getSumOfDepthOfHoles);
+        features.add(Features::getMeanAbsoluteDeviationOfTop);
+        features.add(Features::getBlocksAboveHoles);
+        features.add(Features::getSignificantHoleAndTopDifference);
+        features.add(Features::getNumOfSignificantTopDifference);
+        features.add(Features::hasLevelSurface);
+        features.add(Features::getNumColsWithHoles);
+        features.add(Features::getNumRowsWithHoles);
+        Features.addAllColHeightFeatures(features);
+        Features.addAllHeightDiffFeatures(features);
+        features.add(Features::getBumpiness);
+        features.add(Features::getTotalHeight);
 
-        //Populating random weights
+        /**
+         *  Adding increasing decreasing trainer's champion with trimmedFeatures
+         */
+
+        double[] championArray = {6.0, 0.5, -8.0, 0.0, 8.0, 0.0, 0.5, 26.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                2.0, 0.0, 0.0, 0.5, -1.0, 2.0, -2.0, 0.0, 0.0, 0.0, 0.0, 0.5, 0.0, 0.0, 3.2, 32.0};
+
+        System.out.println("Full feature list size: " + features.size());
+        System.out.println("championArray size: " + championArray.length);
+
+        oneSetCoefficients = new ArrayList<>();
+
+        for (int i = 0; i < championArray.length; i++) {
+
+            double currentValue = championArray[i];
+
+            if (currentValue != 0.0) {
+                trimmedFeatures.add(features.get(i));
+                oneSetCoefficients.add(currentValue);
+            }
+        }
+
+        playerCoefficients.add(oneSetCoefficients);
+
+        System.out.println("Trimmed feature list size: " + trimmedFeatures.size());
+        System.out.println("coefficient size: " + oneSetCoefficients.size());
+
+        /**
+         * Populating random coefficients
+         */
         for (int i = 0; i < numPlayers; i++) {
-            oneSetCoefficients = new ArrayList<Double>();
-            for (int j = 0; j < features.size(); j++) {
-                oneSetCoefficients.add(ThreadLocalRandom.current().nextDouble(0,10));
+            oneSetCoefficients = new ArrayList<>();
+            for (int j = 0; j < trimmedFeatures.size(); j++) {
+                oneSetCoefficients.add(ThreadLocalRandom.current().nextDouble(-10,10));
             }
             playerCoefficients.add(oneSetCoefficients);
         }
 
         for (int generation = 1; generation < 20; generation++) {
-            resultPair = oneGenerationRun(playerCoefficients, features);
+            resultPair = oneGenerationRun(playerCoefficients, trimmedFeatures);
             if (generation < 19) {
                 playerCoefficients = crossBreed(resultPair, playerCoefficients);
             }
-
         }
 
         System.out.println("Winner winner chicken dinner: " + playerCoefficients.get(resultPair[0]));
